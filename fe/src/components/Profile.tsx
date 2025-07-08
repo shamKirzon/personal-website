29d0d68494b40980e7ad08d4a772f9770110ed96
shammy14-1 <shammysuyat@gmail.com>
2025-07-08 19:33:41 +0800

    feat: responsive

--- fe/src/components/Profile.tsx
+++ fe/src/components/Profile.tsx
@@ -19,44 +19,48 @@ const Profile = () => {
     // parent
     <>
       {/*Content Container*/}
-      <div className="flex justify-center ">
+      <div className="flex justify-center">
         {/* Profile, Text Container */}
-        <div className="flex gap-x-3 items-center sm: mt-4 lg:gap-x-5">
+        <div className="flex gap-x-3   sm: mt-4 lg:gap-x-5">
           <img
             src={myProfile}
             alt="Profile"
-            className="max-h-24 rounded-md h-auto sm:min-h-30 lg:max-h-50 "
+            className="max-h-24 rounded-md h-auto sm:min-h-35 lg:max-h-50 "
           />
 
           {/* text, button div  */}
-          <div className="flex flex-col justify-center pr-4  sm:w-[22rem] lg:w-[25rem] lg:gap-y-6">
+          <div className="flex flex-col  pr-4 w-[13rem] sm:w-[22rem] lg:w-[25rem] lg:gap-y-6">
+            
             {/* name, location, description */}
-            <div className="flex flex-col gap-y-1">
-              <h1 className="text-white text-md sm:text-xl lg:text-3xl lg:font-bold ">
-                Shammy Kierson Suyat
-              </h1>
-
-              {/*location */}
-
-              <div className="flex items-center">
-                <img
-                  src={location}
-                  alt={location}
-                  className=" sm:w-5 lg:w-6 "
-                />
-
-                <h1 className="text-white text-sm sm:text-xl lg:font-extralight lg:text-[1rem]">
-                  Taguig City, Philippines
+            <div className="flex flex-col">
+              
+              {/*name,location  */}
+              <div>
+                <h1 className="text-white text-md h-5 font-bold sm:text-[1.5rem] lg:text-3xl lg:font-bold ">
+                  Shammy Kierson Suyat
                 </h1>
+
+                {/*location */}
+                <div className="flex items-center sm:mt-3">
+                  <img
+                    src={location}
+                    alt={location}
+                    className=" w-[.8rem] sm:w-5 lg:w-6 "
+                  />
+
+                  <h1 className="text-white font-extralight text-[.5rem] text sm:text-sm lg:text-[1rem]">
+                    Taguig City, Philippines
+                  </h1>
+                </div>
               </div>
-              <p className="text-[#A1A1AA] text-[clamp(0.5rem,2.5vw,0.7rem)] sm:text-[.9rem] lg:text-[1rem] lg:mt-2.5 ">
+              <p className="text-[#A1A1AA] mt-1 text-[0.5rem] sm:text-[.9rem] lg:text-[1rem] lg:mt-2.5 ">
                 Computer Science student focused on learning full-stack
                 development.
               </p>
             </div>
 
             {/* button */}
-            <div className="flex gap-x-2 sm:gap-x-5 ">
+            <div className="flex mt-2.5 gap-x-2 sm:gap-x-5 ">
               {profileButtons.map((button, index) => (
                 <button
                   key={index}
@@ -69,9 +73,9 @@ const Profile = () => {
                   <img
                     src={button.icon}
                     alt={button.icon}
-                    className=" sm:w-5 lg:w-7 "
+                    className=" w-3 sm:w-5 lg:w-7 "
                   />
-                  <p className="text-[clamp(0.5rem,2.5vw,0.5rem)] text-white sm:text-[.7rem] lg:text-[.8rem] ">
+                  <p className="text-[.5rem] text-white sm:text-[.7rem] lg:text-[.8rem] ">
                     {button.label}
                   </p>
                 </button>
