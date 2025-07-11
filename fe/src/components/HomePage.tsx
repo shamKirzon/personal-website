74f4cdc0701b1989f3e7cf4f4a2708b347d1590c
shammy14-1 <shammysuyat@gmail.com>
2025-07-12 00:41:16 +0800

    feat: project transition

--- fe/src/components/HomePage.tsx
+++ fe/src/components/HomePage.tsx
@@ -1,24 +1,21 @@
-import Profile from "./Profile"
-import About from "./About"
-import TechStack from "./TechStack"
-import Education from "./Education"
-import Work from "./Work"
-import Project from "./Project"
-
-
+import Profile from "./Profile";
+import About from "./About";
+import TechStack from "./TechStack";
+import Education from "./Education";
+import Work from "./Work";
+import Project from "./Project";
 
 const HomePage = () => {
-
   return (
     <div className="p-4">
-        <Profile/>
-        <About/>
-        <TechStack/>
-        <Education/> 
-        <Work/>
-        <Project/>
+      <Profile />
+      <About />
+      <TechStack />
+      <Education />
+      <Project />
+      <Work />
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
