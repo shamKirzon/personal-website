78cc53573ae5d931e7aca101ba01194c5a55977f
Shammy Kierson Suyat <shammysuyat@gmail.com>
2026-07-30 01:51:17 +0800

    merge

--- fe/src/pages/ProjectDetailPage.tsx
+++ fe/src/pages/ProjectDetailPage.tsx
@@ -4,11 +4,7 @@ import { ArrowLeft, X } from "lucide-react";
 import { project } from "@/data/Project-data";
 import Pill from "@/components/ui/Pill";
 import { LaptopMockup, PhoneMockup } from "@/components/ui/DeviceMockup";
-<<<<<<< HEAD
-import { StaggerGroup, StaggerItem } from "@/components/ui/Reveal";
-=======
 import { StaggerGroup, itemVariants } from "@/components/ui/Reveal";
->>>>>>> dev
 
 const ProjectDetailPage = () => {
   const { slug } = useParams();
@@ -103,15 +99,9 @@ const ProjectDetailPage = () => {
         </p>
         <StaggerGroup className="flex flex-wrap gap-2.5">
           {data.technology.map((tech) => (
-<<<<<<< HEAD
-            <StaggerItem key={tech.name}>
-              <Pill>{tech.name}</Pill>
-            </StaggerItem>
-=======
             <Pill key={tech.name} variants={itemVariants}>
               {tech.name}
             </Pill>
->>>>>>> dev
           ))}
         </StaggerGroup>
       </section>
